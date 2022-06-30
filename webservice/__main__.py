3b4d3ee001dd21752e78c0dacfd5eca35fd38524
Teo Podobnik <teo.podobnik1234@gmail.com>
2022-06-30 17:20:45 +0200

    feat: initial webhook

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -1,11 +1,38 @@
 import os
+import aiohttp
 from aiohttp import web
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
+from datetime import datetime, date
 
+router = routing.Router()
 routes = web.RouteTableDef()
+
+USER = "tp4348"
+REPOSITORY = "DemoPrivateRepository"
 
 @routes.get("/")
 async def main(request):
-    return web.Response(status=200, text="Hello world!")
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, USER, oauth_token=os.getenv("GH_TOKEN"))
+        branches = await gh.getitem(f'/repos/{USER}/{REPOSITORY}/branches')
+        # For each branch
+        for b in branches:
+            name = b['name']
+            branch = await gh.getitem(f'/repos/{USER}/{REPOSITORY}/branches/{name}')
+            timedate = branch['commit']['commit']['author']['date']
+            dateoflastcommit,_  = timedate.split("T")
+            today = str(date.today())
+            dateoflastcommit = datetime.strptime(dateoflastcommit, "%Y-%m-%d")
+            today = datetime.strptime(today, "%Y-%m-%d")
+            delta = today - dateoflastcommit
+            if (delta.days > 1) and name != 'main':
+                deletion = await gh.delete(f'/repos/{USER}/{REPOSITORY}/git/refs/heads/{name}')
+                print(f"Delete branch {name} in repository {REPOSITORY}. {user} made me do it, I'm sorry :(")
+            else:
+                print("I was called, but I didn't do shiet")
+
+    return web.Response(status=200)
 
 if __name__ == "__main__":
     app = web.Application()
